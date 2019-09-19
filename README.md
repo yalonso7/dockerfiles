f7a674f1ebc99d95d38b9dafeff18082ea429a03
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2019-09-20 00:04:29 +0300

    start of repo info branch

---
+++ README.md
@@ -0,0 +1,7 @@
+# Demisto's Docker Repository Information
+
+This branch contains pakcage and os information about each docker image available via Demisto's Docker Hub repositories at:  https://hub.docker.com/u/demisto/ .
+
+Each information file is maintained as a markdown file in its relevant folder. The last available tag of each docker image is kept in a file named `last.md` in the a folder named as the docker image.
+
+## Docker Image List
---
+++ update-docker-repo-info.py
@@ -0,0 +1,296 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+import os
+import requests
+import datetime
+import string
+import subprocess
+import tempfile
+import shutil
+import json
+import re
+import random
+
+assert sys.version_info >= (3, 7), "Script compatible with python 3.7 and higher only"
+
+
+def get_latest_tag(image_name):
+    last_tag = None
+    last_date = None
+    url = "https://registry.hub.docker.com/v2/repositories/{}/tags/?page_size=25".format(image_name)
+    while True:
+        print("Querying docker hub url: {}".format(url))
+        res = requests.get(url)
+        res.raise_for_status()
+        obj = res.json()
+        for result in obj['results']:
+            name = result['name']
+            if len(name) >= 20 and all(c in string.hexdigits for c in name):  # skip git sha revisions
+                continue
+            date = datetime.datetime.strptime(result['last_updated'], "%Y-%m-%dT%H:%M:%S.%fZ")
+            if not last_date or date > last_date:
+                last_date = date
+                last_tag = result['name']
+        if obj['next']:
+            url = obj['next']
+        else:
+            break
+    print("last tag: {}, date: {}".format(last_tag, last_date))
+    if not last_tag:
+        raise Exception('No tag found for image: {}'.format(image_name))
+    return (last_tag, last_date)
+
+
+def inspect_image(image_name, out_file):
+    inspect_format = '''
+## Docker Metadata
+- Image ID: `{{ .Id }}`
+- Created: `{{ .Created }}`
+- Arch: `{{ .Os }}`/`{{ .Architecture }}`
+{{ if .Config.Entrypoint }}- Entrypoint: `{{ json .Config.Entrypoint }}`
+{{ end }}{{ if .Config.Cmd }}- Command: `{{ json .Config.Cmd }}`
+{{ end }}- Environment:{{ range .Config.Env }}{{ "\\n" }}  - `{{ . }}`{{ end }}
+- Labels:{{ range $key, $value := .ContainerConfig.Labels }}{{ "\\n" }}  - `{{ $key }}:{{ $value }}`{{ end }}
+'''
+    docker_info = subprocess.check_output(["docker", "inspect", "-f", inspect_format, image_name], text=True)
+    out_file.write(docker_info + "\n")
+
+
+PKG_INFO = '''
+### `{name}`
+
+* Summary: {summary}
+* Version: {version}
+* Pypi: {pypi_url}
+* Homepage: {home_page}
+* Author: {author}
+'''
+
+USED_PACKAGES_FILE = "used_packages.json"
+USED_PACKAGES = {}
+
+
+def clear_image_from_used(base_image):
+    """
+    Remove the image from all packages referencing this image. Should be called at the
+    start of processing of an image
+    """
+    to_remove = []
+    for name, package in USED_PACKAGES.items():
+        if base_image in package["docker_images"]:
+            package["docker_images"].remove(base_image)
+            if len(package["docker_images"]) == 0:
+                to_remove.append(name)
+    for name in to_remove:
+        USED_PACKAGES.pop(name)
+
+
+def add_package_used(package_name, base_image, licenses, home_page, pypi_url, summary, author):
+    package = USED_PACKAGES.get(package_name)
+    if package:
+        if base_image not in package["docker_images"]:
+            package["docker_images"].append(base_image)
+    else:
+        USED_PACKAGES[package_name] = {
+            "docker_images": [base_image],
+            "home_page": home_page,
+            "pypi_url": pypi_url,
+            "author": author,
+            "summary": summary,
+            "licenses": licenses
+        }
+
+
+def generate_pkg_data(image_name, out_file):
+    if not hasattr(generate_pkg_data, "cache"):
+        generate_pkg_data.cache = {}
+        res = requests.get(
+            'https://raw.githubusercontent.com/demisto/dockerfiles/master/docker/known_licenses.json?{}'.format(random.randint(1, 1000)))
+        res.raise_for_status()
+        generate_pkg_data.known_licenses = res.json()["packages"]
+        with open("{}/packages_ignore.json".format(sys.path[0])) as f:
+            generate_pkg_data.ignore_packages = json.load(f)["packages"]        
+    # check this image is python
+    try:
+        subprocess.check_output(["docker", "run", "--rm", image_name, "which", "python"], text=True)
+    except subprocess.CalledProcessError as err:
+        if err.returncode == 1:
+            print("{} doesn't seem to have python. Skipping python package check. {}".format(image_name, err.output))
+            return
+        raise
+    base_image = image_name.split(":")[0]
+    clear_image_from_used(base_image)
+    pip_list_json = subprocess.check_output(["docker", "run", "--rm", image_name, "sh", "-c",
+                                            "pip install --upgrade pip > /dev/null; python -m pip list --format=json"], text=True)
+    pip_list = json.loads(pip_list_json)
+    for pkg in pip_list:
+        name = pkg["name"]
+        if (name in generate_pkg_data.ignore_packages and
+                ((not generate_pkg_data.ignore_packages[name].get("docker_images")) or
+                    base_image in generate_pkg_data.ignore_packages[name].get("docker_images"))):
+            print("Ignoring package: " + name)
+            continue
+        print("Getting license for package: {} ...".format(name))
+        pip_info = generate_pkg_data.cache.get(name)
+        pip_show = None
+        if not pip_info:
+            try:
+                res = requests.get("https://pypi.org/pypi/{}/json".format(name))
+                res.raise_for_status()
+                pip_info = res.json()
+                generate_pkg_data.cache[name] = pip_info
+            except Exception as ex:
+                print("Failed getting info from pypi (will try pip): " + str(ex))
+                pip_show = subprocess.check_output(["docker", "run", "--rm", image_name, "pip", "show", name], text=True)
+                inner_info = {}
+                for line in pip_show.splitlines():
+                    values = line.split(":", 1)
+                    if len(values) > 1:
+                        line_name, val = values
+                        inner_info[line_name.lower().replace('-', '_').strip()] = val.strip()
+                pip_info = {"info": inner_info}
+        else:
+            print("Using cache for license data of package: " + name)
+        home_page = pip_info["info"].get("home_page")
+        author = pip_info["info"].get("author")
+        author_email = pip_info["info"].get("author_email")
+        if author_email and author_email.lower() != 'unknown':
+            if not author:
+                author = author_email
+            else:
+                author = author + ' ' + author_email
+        summary = pip_info["info"].get("summary")
+        pypi_url = pip_info["info"].get("package_url")
+        out_file.write(PKG_INFO.format(name=name, summary=summary, version=pkg["version"],
+                       pypi_url=pypi_url, home_page=home_page, author=author))
+        classifier_found = False
+        classifier_list = None
+        if name in generate_pkg_data.known_licenses:
+            classifier_list = [generate_pkg_data.known_licenses[name]["license"]]
+        else:
+            classifier_list = pip_info["info"].get("classifiers")
+        found_licenses = []
+        if classifier_list:
+            for classifier in classifier_list:
+                if classifier.startswith("License ::") and classifier != "License :: OSI Approved":
+                    classifier_found = True
+                    found_licenses.append(classifier)
+                    out_file.write("* {}\n".format(classifier))                    
+        if not classifier_found:
+            # try getting license via pip show
+            if not pip_show:
+                pip_show = subprocess.check_output(["docker", "run", "--rm", image_name, "pip", "show", name], text=True)
+            for line in pip_show.splitlines():
+                if line.startswith("License:"):
+                    out_file.write("* {}\n".format(line))
+                    found_licenses.append(line)                    
+        add_package_used(name, base_image, found_licenses, home_page, pypi_url, summary, author)
+
+
+def list_os_packages(image_name, out_file):
+    res = subprocess.run(["docker", "run", "--rm", image_name, "sh", "-c", "grep -i alpine /etc/issue"],
+        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)  # noqa
+    if res.returncode == 0:  # alpine
+        output = subprocess.check_output(["docker", "run", "--rm", image_name, "sh", "-c",
+            "apk list -I | sort"], text=True, stderr=subprocess.DEVNULL)  # noqa
+        os_pkgs = [" ".join(l.split()[0:3]) for l in output.splitlines()]
+    else:
+        output = subprocess.check_output(["docker", "run", "--rm", image_name, "sh", "-c",
+            "dpkg-query --show | sort"], text=True)  # noqa
+        os_pkgs = output.splitlines()    
+    md_lines = ["* " + x for x in os_pkgs]
+    out_file.write("\n".join(md_lines))
+    out_file.write("\n")
+
+
+def process_image(image_name, force):
+    print("=================\nProcessing: " + image_name)
+    last_tag, last_date = get_latest_tag(image_name)
+    full_name = "{}:{}".format(image_name, last_tag)
+    dir = "{}/{}".format(sys.path[0], image_name)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    info_file = "{}/{}.md".format(dir, last_tag)
+    last_file = "{}/last.md".format(dir)
+    if not force and os.path.exists(info_file):
+        print("Info file: {} exists skipping image".format(info_file))
+        return
+    print("Downloading docker image: {}...".format(full_name))
+    subprocess.call(["docker", "pull", full_name])
+    temp_file = tempfile.NamedTemporaryFile(mode='w', encoding='utf-8', delete=False)
+    print("Using temp file: " + temp_file.name)
+    try:
+        temp_file.write("# `{}:{}`\n".format(image_name, last_tag))
+        inspect_image(full_name, temp_file)
+        temp_file.write("## `Python Packages`\n\n")
+        generate_pkg_data(full_name, temp_file)
+        temp_file.write("\n## `OS Packages`\n\n")
+        list_os_packages(full_name, temp_file)
+        temp_file.close()
+        shutil.move(temp_file.name, info_file)
+        shutil.copy(info_file, last_file)
+    except Exception as e:
+        print("Error: {}".format(e))
+        if isinstance(e, subprocess.CalledProcessError):
+            print("Stderr: {}".format(e.stderr))
+        os.remove(temp_file.name)
+        raise
+
+
+def process_org(org_name, force):
+    url = "https://registry.hub.docker.com/v2/repositories/{}/?page_size=25".format(org_name)
+    with open("{}/images_ignore.txt".format(sys.path[0])) as f:
+        ignore_list = list(filter(lambda x: not x.startswith('#'), f.read().splitlines()))
+        print("ingore list: {}".format(ignore_list))
+    while True:
+        print("Querying docker hub url: {}".format(url))
+        res = requests.get(url)
+        res.raise_for_status()
+        obj = res.json()
+        for result in obj['results']:
+            name = "{}/{}".format(org_name, result['name'])
+            if name not in ignore_list:
+                process_image(name, force)
+        if obj['next']:
+            url = obj['next']
+        else:
+            break
+
+
+def generate_readme_listing():
+    with open('README.md', "r") as f:
+        readme_lines = f.readlines()
+    list_title_indx = readme_lines.index('## Docker Image List')
+    trunc_readme = readme_lines[0:list_title_indx+1]
+    with open('README.md', 'w') as f:
+        f.writelines(trunc_readme)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Fetch docker repo info. Will fetch the docker image and then generate license info',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("docker_image", help="The docker image name to use (ie: demisto/python). Optional." +
+                        "If not specified will scan all images in the demisto organization", nargs="?")
+    parser.add_argument("--force", help="Force refetch even if license data already exists", action='store_true')
+    args = parser.parse_args()
+    global USED_PACKAGES
+    used_packages_path = "{}/{}".format(sys.path[0], USED_PACKAGES_FILE)
+    if os.path.isfile(used_packages_path):
+        with open(used_packages_path) as f:
+            USED_PACKAGES = json.load(f)
+    try:
+        if args.docker_image:
+            process_image(args.docker_image, args.force)
+        else:
+            process_org("demisto", args.force)
+    finally:
+        with open(used_packages_path, "w") as f:
+            json.dump(USED_PACKAGES, f, sort_keys=True,
+                      indent=4, separators=(',', ': '))
+    generate_readme_listing()
+
+
+if __name__ == "__main__":
+    main()
